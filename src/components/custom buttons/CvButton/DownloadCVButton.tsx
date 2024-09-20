a47100bee04e8af620b8afe89a28ad2596dea73e
Anastasiya145 <a.ivanova-external@powernext.com>
2024-09-20 14:35:44 +0200

    fixes

--- src/components/custom buttons/CvButton/DownloadCVButton.tsx
+++ src/components/custom buttons/CvButton/DownloadCVButton.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
 import "./download-cv-button.scss";
 
 const DownloadCVButton: FC = () => {
-  const { t } = useTranslation("navbar");
-  const resumeLink = "URL_TO_YOUR_PDF"; //fix later
+  const { t, i18n } = useTranslation("navbar");
+
+  const resumeLink =
+    i18n.language === "fr"
+      ? "https://drive.google.com/uc?export=download&id=1oM8bIhzKNnOJy7VpumlC12GfVvTyTpVR"
+      : "https://drive.google.com/uc?export=download&id=1MsNlpziDCVHHLQ5mCD1MvidurxFszsC_";
 
   return (
     <div className="download-cv">
-      <Link
+      <a
         className="download-cv__button button is-link"
-        to={resumeLink}
+        href={resumeLink}
         download
         aria-label="Download Resume"
       >
@@ -19,7 +22,7 @@ const DownloadCVButton: FC = () => {
           <i className="fas fa-download"></i>
         </span>
         <span>{t("downloadCV")}</span>
-      </Link>
+      </a>
     </div>
   );
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,5 @@
-const BASE_PATH = "/portfolio_bulma";
-// const BASE_PATH = "";
-
 export const routes = {
-  home: `${BASE_PATH}/`,
-  about: `${BASE_PATH}/about-me`,
-  portfolio: `${BASE_PATH}/portfolio`,
+  home: "/",
+  about: "/about-me",
+  portfolio: "/portfolio",
 };
